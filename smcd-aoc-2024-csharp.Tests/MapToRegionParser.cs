f94ab1e7774d4535f9b492f9d1d857834e193f3a
Niklas Rueber <nrueber@igus.net>
2024-12-19 17:40:34 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:smcd-aoc-2024-csharp.Tests/MapToRegionParser.cs

--- smcd-aoc-2024-csharp.Tests/MapToRegionParser.cs
+++ smcd-aoc-2024-csharp.Tests/MapToRegionParser.cs
@@ -4,10 +4,20 @@ static class MapToRegionParser {
     public static List<Region> Parse(string map)
     {
         var regions = new List<Region>();
-
-        foreach ( var item in map) {
+        var column = 0;
+        while(column < map.Length)
+        {
+            var plantType = map[column];
+            
             var region = new Region(1, 4);
+            column++;
+            do
+            {
+
+            } while (plantType = newPlantType);
+            
             regions.Add ( region );
+            column++;
         }
 
         return regions;
